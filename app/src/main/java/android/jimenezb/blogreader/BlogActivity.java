c40161c75bb9e5dbb1f82e790985228c596665c2
Brandon Jimenez <jimenezchronic@yahoo.com>
2014-10-08 13:28:11 -0700

    I used an adapter to display the list items

--- app/src/main/java/android/jimenezb/blogreader/BlogActivity.java
+++ app/src/main/java/android/jimenezb/blogreader/BlogActivity.java
@@ -3,6 +3,7 @@ package android.jimenezb.blogreader;
 import
         android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
@@ -18,10 +19,21 @@ public class BlogActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog);
 
+        String [] arraystrings = new String[]{
+               "Blog Post 1",
+               "Blog Post 2",
+               "Blog Post 3",
+               "Blog Post 4",
+
+        };
+
         progressBar = (ProgressBar)findViewById(R.id.progressBar);
         listview = (ListView)findViewById(R.id.listview);
 
         listview.setEmptyView(progressBar);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arraystrings);
+        listview.setAdapter(adapter);
     }
 
 }
