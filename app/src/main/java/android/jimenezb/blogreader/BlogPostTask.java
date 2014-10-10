2cb06ba90d25598a2be5351544f513bfc10bf6d1
Brandon Jimenez <jimenezchronic@yahoo.com>
2014-10-10 13:52:43 -0700

    Created a blog post

---
+++ app/src/main/java/android/jimenezb/blogreader/BlogPostTask.java
@@ -0,0 +1,14 @@
+package android.jimenezb.blogreader;
+
+
+import android.app.Activity;
+import android.os.AsyncTask;
+
+import org.json.JSONObject;
+
+public class BlogPostTask extends AsyncTask<Activity, Void, JSONObject>{
+    @Override
+    protected JSONObject doInBackground(Activity... params) {
+        return null;
+    }
+}
